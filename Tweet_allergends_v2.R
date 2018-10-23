e4f48f35da950ce96a47b01e1db29b31e386a5de
lualeperez <lualeperez@gmail.com>
2018-10-23 16:20:30 +0200

    Added dropping of unuseful columns

--- Tweet_allergends_v2.R
+++ Tweet_allergends_v2.R
@@ -1,9 +1,9 @@
-setwd("/home/aperez/Documentos_importantes/Cosas_Importantes/Reconversion_Profesional/S2DS_Virtual_2018/FSA_project/MyCode")
+setwd("/home/aperez/Documentos_importantes/Cosas_Importantes/Reconversion_Profesional/S2DS_Virtual_2018/FSA_project/Code/FSA-Virtual-Oct18/")
 
 source("LoadingData.R")
 
 # path where the data is located
-dataDir   = "../Data/"
+dataDir   = "Data/"
 # list for file names
 filenames = c("20161029-20171127.xlsx",
               "20171128-20180926.xlsx")
@@ -19,5 +19,21 @@ cat(paste("N records = ",nrow(df),"\n",sep=""))
 # Now some preprocessing
 
 # Lets drop some spureous columns
-#columns_to_drop = vector()
-#df <- df[,-columns_to_drop]
+columns_to_drop <- c("search",
+                     "language",
+                     "main emotion",
+                     "emotions",
+                     "image tags",
+                     "tags",
+                     "parent source identifier",
+                     "user longitude",
+                     "user latitude",
+                     "city",
+                     "user city",
+                     "no. of likes",
+                     "no. of comments",
+                     "no. of engagements")
+
+df <- df[,-match(columns_to_drop,names(df))]
+print(names(df))
+
