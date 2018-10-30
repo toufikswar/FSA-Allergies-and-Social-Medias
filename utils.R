6632dd6728e394231afd675da46760b6d32b70f6
qoldt <a.g.newman@gmail.com>
2018-10-30 19:11:34 +0100

    Added Allergen Dictionaries to utils.R

--- utils.R
+++ utils.R
@@ -1,4 +1,8 @@
-# Scrip for loading the data
+# Utilities and Dictionaries
+
+## FUNCTIONS:
+
+### LOADING DATA:
 
 library(readxl)          # library to read xlsx files (excel)
 
@@ -49,6 +53,9 @@ load_list_of_xlsx_files = function(filenames,verbose=FALSE)
 
 }
 #================================================================
+
+### TESTING ORIGINAL TWEET VS PROCESSED TWEET
+
 test_text_preprocessing = function(original.data,preprocessed.data,n.test.records)
 {
 
@@ -83,4 +90,47 @@ test_text_preprocessing = function(original.data,preprocessed.data,n.test.record
 
 
 
-#
+## DICTIONARIES (will only create if library(quanteda) is loaded correctly)
+
+fourteen_allergens.dict <- dictionary(list(celery = c("celery*"),
+                                           cereals_contain_gluten = c("gluten*", "wheat*", "rye*", "barley*", "oat*"),
+                                           crustaceans = c("crab*", "prawn*", "lobster*",  "scampi*",  "shrimp*", "shrimp paste", "shrimppaste", "crayfish*", "cray fish*", "crustacean*"),
+                                           eggs = c("egg*"),
+                                           fish = c("fish*", "salmon*", "tuna*"),
+                                           lupin = c("lupin*"),
+                                           milk = c("milk*"),
+                                           molluscs = c("mollusc*", "mussel*", "oyster*", "clams*", "scallop*", "snail*", "squid*"),
+                                           mustard = c("mustard*"),
+                                           tree_nuts = c("nut*" ,"almond*", "hazelnut*", "walnut*", "brazil nut*", "brazilnut*", "cashew*", "pecan*", "pistachio*", "macadamia nut*", "macadamianut*", "queensland nut*", "queenslandnut*", "tree nut*", "treenut*"),
+                                           peanuts = c("peanut*", "pea nut*"),
+                                           sesame_seeds = c("sesame seed*", "sesameseed*"),
+                                           soybeans = c("soya*", "soybean*", "soy bean*", "soy*"),
+                                           sulphur_dioxide_and_sulphites = c("sulphur dioxide*", "sulphurdioxide*", "sulphit*", "sulphite*"))
+)
+
+
+other_allergens.dict <- dictionary(list(corn = c("corn*", "maize*",  "sweetcorn*"),
+                                        meat = c("meet*", "pork*", "beef*", "lamb*"),
+                                        gelatine = c("gelatine*"), 
+                                        seed = c("seed*", "sunflower*", "poppy*", "seed oil*"),
+                                        spice = c("spice*", "coriander*", "garlic*", "cinnamon*", "chilli*"),
+                                        fruit = c("fruit*", "apple*", "carrot*", "peach*", "plum*", "tomato*", "banana*", "strawberry*" ),
+                                        vegetable = c("vegetable*"),
+                                        latex = c("latex*"),
+                                        carrageenan = c("carrageenan*", "seaweed*", "sea weed*"),
+                                        coconut = c("coconut*"),
+                                        chestnut = c("chestnut*", "chest nut*"),
+                                        pinenut = c("pinenut*", "pine nut*"),
+                                        fungus = c("fungus*", "quorn*", "yeast*", "mushroom*"),
+                                        kiwi = c("kiwi*"),
+                                        peas = c("peas*"),
+                                        chickpeas = c("chickpeas*", "chick peas*"),
+                                        poppy_seeds = c("poppy seed*", "poppyseed*"),
+                                        pepper = c("pepper*", "bell pepper*"),
+                                        pumpkin = c("pumpkin*", "squash*", "punkin*"),
+                                        kidney_beans = c("kidney bean*", "kidneybean*"),
+                                        garlic = c("garlic*"),
+                                        banana = c("banana*"),
+                                        lentils = c("lentil*"),
+                                        edible_insects = c("edible instect*", "edible bug*", "cricket*", "grasshopper*", "mealworm*",  "buffalo worm*", "buffaloworm*"))
+)
