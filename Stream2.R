325155ccc3b650096e75bec7e894aefdbe5dd728
qoldt <a.g.newman@gmail.com>
2018-11-01 18:40:44 +0100

    Added Time series plots

--- Stream2.R
+++ Stream2.R
@@ -21,10 +21,10 @@ other_allergens.df.norm <- data.frame(id = other_allergens.df$id, ifelse(other_a
 
 # Merge labelled tweets with metadata.df
 library(dplyr)
-fourteen_allergens.df <- left_join(fourteen_allergens.df, metadata.df[, c("id", "source", "latitude", "longitude","date","users","hashtags")], "id")
-fourteen_allergens.df.norm <- left_join(fourteen_allergens.df.norm, metadata.df[, c("id", "source", "latitude", "longitude","date","users","hashtags")], "id")
-other_allergens.df <- left_join(other_allergens.df, metadata.df[, c("id", "source", "latitude", "longitude","date","users","hashtags")], "id")
-other_allergens.df.norm <- left_join(other_allergens.df.norm, metadata.df[, c("id", "source", "latitude", "longitude","date","users","hashtags")], "id")
+fourteen_allergens.df <- left_join(fourteen_allergens.df, metadata.df[, c("id", "source", "latitude", "longitude","date","users","hashtags", "sentiment class")], "id")
+fourteen_allergens.df.norm <- left_join(fourteen_allergens.df.norm, metadata.df[, c("id", "source", "latitude", "longitude","date","users","hashtags", "sentiment class")], "id")
+other_allergens.df <- left_join(other_allergens.df, metadata.df[, c("id", "source", "latitude", "longitude","date","users","hashtags","sentiment class")], "id")
+other_allergens.df.norm <- left_join(other_allergens.df.norm, metadata.df[, c("id", "source", "latitude", "longitude","date","users","hashtags","sentiment class")], "id")
 
 
 library(tidyr)
@@ -46,6 +46,17 @@ other_allergens.df.norm.long <- other_allergens.df.norm
 other_allergens.df.norm.long <- gather(other_allergens.df.norm.long, Allergen, "Mentions", other.allergen.names, factor_key = TRUE)
 other_allergens.df.norm.long$category <- "Other_Allergens"
 other_allergens.df.norm.long$class <- "per_document"
+
+all_allergens.df <- rbind(fourteen.df.long, other_allergens.df.long)
+all_allergens.df <- all_allergens.df[all_allergens.df$Mentions!=0,] # remove rows with no mentions
+all_allergens.df$date <- as.Date(all_allergens.df$date, format= "%Y-%m-%d")
+
+all_allergens.norm.df <- rbind(fourteen.df.norm.long, other_allergens.df.norm.long)
+all_allergens.norm.df$date <- as.Date(all_allergens.norm.df$date, format= "%Y-%m-%d")
+#all_allergens.norm.df <- all_allergens.norm.df[all_allergens.norm.df$Mentions!=0,] # to remove zeros. When binning (ie. by month) dont perform this and keep zeros in.
+all_allergens.norm.df$Month <- as.Date(cut(all_allergens.norm.df$date, breaks = "month"))
+names(all_allergens.norm.df)[names(all_allergens.norm.df) == "sentiment class"] <- "sentiment_class" #rename sentiment class to a single string
+all_allergens.norm.df$sentiment_class[all_allergens.norm.df$sentiment_class %in% c("not_evaluable", "processing")] <- "neutral" # collapse not evaluable and procesing to neutral
 
 
 fourteen.bysource.df <- fourteen.df.long %>%
@@ -122,3 +133,28 @@ bysource.panel <- ggarrange(fourteen.bysource, fourteen.bysource.norm,
                             other.bysource, other.bysource.norm,
                             ncol = 2, nrow = 2)
 bysource.panel
+
+
+library(scales)
+library(ggrepel)
+
+all_allergens.norm.df.t14 <- subset(all_allergens.norm.df, source == "Twitter" & Allergen %in% fourteen.allergen.names)
+
+by.date.twitter.14 <- ggplot(all_allergens.norm.df.t14, aes(x = Month, y = Mentions, colour = Allergen), group = Allergen)+
+  stat_summary(fun.y = sum, # adds up all observations for the month
+               geom = "line") +
+  theme_minimal()+
+  theme(legend.position="bottom")+
+  facet_grid(sentiment_class~.)
+by.date.twitter.14
+
+all_allergens.norm.df.t.other <- subset(all_allergens.norm.df, source == "Twitter" & Allergen %in% other.allergen.names)
+
+by.date.twitter.other <- ggplot(all_allergens.norm.df.t.other, aes(x = Month, y = Mentions, colour = Allergen), group = Allergen)+
+  stat_summary(fun.y = sum, # adds up all observations for the month
+               geom = "line") +
+  theme_minimal()+
+  theme(legend.position="bottom")+
+  facet_grid(sentiment_class~.)
+by.date.twitter.other
+
