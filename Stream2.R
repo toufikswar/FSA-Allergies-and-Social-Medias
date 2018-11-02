5565d32206bc015d626a6601bb915f698d228543
qoldt <a.g.newman@gmail.com>
2018-11-02 14:27:24 +0100

    Cleaned up, removed links to metadata.df & added static plots binned by month or by week

--- Stream2.R
+++ Stream2.R
@@ -19,42 +19,39 @@ other.allergen.names <- colnames(other_allergens.df)[-1]
 # Normalized to one mention per document
 other_allergens.df.norm <- data.frame(id = other_allergens.df$id, ifelse(other_allergens.df[,other.allergen.names] > 0, 1, 0))
 
-# Merge labelled tweets with metadata.df
+# Merge labelled tweets with other features from data.df
 library(dplyr)
-fourteen_allergens.df <- left_join(fourteen_allergens.df, metadata.df[, c("id", "source", "latitude", "longitude","date","users","hashtags", "sentiment class")], "id")
-fourteen_allergens.df.norm <- left_join(fourteen_allergens.df.norm, metadata.df[, c("id", "source", "latitude", "longitude","date","users","hashtags", "sentiment class")], "id")
-other_allergens.df <- left_join(other_allergens.df, metadata.df[, c("id", "source", "latitude", "longitude","date","users","hashtags","sentiment class")], "id")
-other_allergens.df.norm <- left_join(other_allergens.df.norm, metadata.df[, c("id", "source", "latitude", "longitude","date","users","hashtags","sentiment class")], "id")
+fourteen_allergens.df <- left_join(fourteen_allergens.df, data.df[, c("id", "source", "latitude", "longitude","date","users","hashtags", "sentiment class", "content")], "id")
+fourteen_allergens.df.norm <- left_join(fourteen_allergens.df.norm, data.df[, c("id", "source", "latitude", "longitude","date","users","hashtags", "sentiment class","content")], "id")
+other_allergens.df <- left_join(other_allergens.df, data.df[, c("id", "source", "latitude", "longitude","date","users","hashtags","sentiment class","content")], "id")
+other_allergens.df.norm <- left_join(other_allergens.df.norm, data.df[, c("id", "source", "latitude", "longitude","date","users","hashtags","sentiment class","content")], "id")
 
 
 library(tidyr)
 library(forcats)
 fourteen.df.long <- fourteen_allergens.df
 fourteen.df.long <- gather(fourteen.df.long, Allergen, "Mentions", fourteen.allergen.names, factor_key = TRUE)
-fourteen.df.long$category <- "Fourteen_Allergens"
 fourteen.df.long$class <- "raw_counts"
 fourteen.df.norm.long <- fourteen_allergens.df.norm
 fourteen.df.norm.long <- gather(fourteen.df.norm.long, Allergen, "Mentions", fourteen.allergen.names, factor_key = TRUE)
-fourteen.df.norm.long$category <- "Fourteen_Allergens"
 fourteen.df.norm.long$class <- "per_document"
 
 other_allergens.df.long <- other_allergens.df
 other_allergens.df.long <- gather(other_allergens.df.long, Allergen, "Mentions", other.allergen.names, factor_key = TRUE)
-other_allergens.df.long$category <- "Other_Allergens"
 other_allergens.df.long$class <- "raw_counts"
 other_allergens.df.norm.long <- other_allergens.df.norm
 other_allergens.df.norm.long <- gather(other_allergens.df.norm.long, Allergen, "Mentions", other.allergen.names, factor_key = TRUE)
-other_allergens.df.norm.long$category <- "Other_Allergens"
 other_allergens.df.norm.long$class <- "per_document"
 
 all_allergens.df <- rbind(fourteen.df.long, other_allergens.df.long)
-all_allergens.df <- all_allergens.df[all_allergens.df$Mentions!=0,] # remove rows with no mentions
+#all_allergens.df <- all_allergens.df[all_allergens.df$Mentions!=0,] # remove rows with no mentions
 all_allergens.df$date <- as.Date(all_allergens.df$date, format= "%Y-%m-%d")
 
 all_allergens.norm.df <- rbind(fourteen.df.norm.long, other_allergens.df.norm.long)
 all_allergens.norm.df$date <- as.Date(all_allergens.norm.df$date, format= "%Y-%m-%d")
 #all_allergens.norm.df <- all_allergens.norm.df[all_allergens.norm.df$Mentions!=0,] # to remove zeros. When binning (ie. by month) dont perform this and keep zeros in.
 all_allergens.norm.df$Month <- as.Date(cut(all_allergens.norm.df$date, breaks = "month"))
+all_allergens.norm.df$Week <- as.Date(cut(all_allergens.norm.df$date, breaks = "week"))
 names(all_allergens.norm.df)[names(all_allergens.norm.df) == "sentiment class"] <- "sentiment_class" #rename sentiment class to a single string
 all_allergens.norm.df$sentiment_class[all_allergens.norm.df$sentiment_class %in% c("not_evaluable", "processing")] <- "neutral" # collapse not evaluable and procesing to neutral
 
@@ -140,13 +137,23 @@ library(ggrepel)
 
 all_allergens.norm.df.t14 <- subset(all_allergens.norm.df, source == "Twitter" & Allergen %in% fourteen.allergen.names)
 
-by.date.twitter.14 <- ggplot(all_allergens.norm.df.t14, aes(x = Month, y = Mentions, colour = Allergen), group = Allergen)+
+by.date.twitter.14.month <- ggplot(all_allergens.norm.df.t14, aes(x = Month, y = Mentions, colour = Allergen), group = Allergen)+
   stat_summary(fun.y = sum, # adds up all observations for the month
                geom = "line") +
   theme_minimal()+
   theme(legend.position="bottom")+
   facet_grid(sentiment_class~.)
-by.date.twitter.14
+by.date.twitter.14.month
+
+by.date.twitter.14.week <- ggplot(all_allergens.norm.df.t14, aes(x = Week, y = Mentions, colour = Allergen), group = Allergen)+
+  stat_summary(fun.y = sum, # adds up all observations for the month
+               geom = "line") +
+  theme_minimal()+
+  theme(legend.position="bottom")+
+  facet_grid(sentiment_class~.)
+by.date.twitter.14.week
+
+all_allergens.norm.df.t14pos <- subset(all_allergens.norm.df.t14, Mentions > 0)
 
 all_allergens.norm.df.t.other <- subset(all_allergens.norm.df, source == "Twitter" & Allergen %in% other.allergen.names)
 
