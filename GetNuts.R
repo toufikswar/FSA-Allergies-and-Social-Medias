5eb06e480452cb4acae437f1580912bab78b4753
lualeperez <lualeperez@gmail.com>
2018-11-22 11:54:58 +0100

    Commenting GetNuts.R script

--- GetNuts.R
+++ GetNuts.R
@@ -81,30 +81,41 @@ configuration = list(input_dir                = "",
                      do_static_plots          = FALSE,
                      launch_shiny_dash_board  = FALSE)
 
+# generic ouput file folder
 output_dir <- "output_files"
 # If output_dir doesn't exist, create it.
 if(!dir.exists(file.path(output_dir))) dir.create(file.path(output_dir))
 
+# Name of the output static plots folder
 plots_output_dir <- ""
 
+# List with the possible static plot formats
 possible_static_plots_formats <- c("eps","ps","pdf","jpeg","tif","png","bmp","svg")
 
+# Names of the preprocessing and labelling image files
+# used for later access to preprocessed and labelled data
 image_preprocessing <- ""
 image_analysis      <- ""
 
-# nomaliation factors
-norm_factor_businesses <- 100      # mentions per    100  Establishments
-norm_factor_population <- 100000   # mentions per 100000  People
+# nomaliation factors for number of Establishments and population
+norm_factor_businesses <- 100      # numbers are expressed per 100  Establishments
+norm_factor_population <- 100000   # numbers are expressed per 100k People
 
 allergies_and_social_media <- function(config_file)
 {
+
+  # Master function controlling the pipeline of allergiesand social media analysis
+  # It reads the input configuration file to define
+  # - The data to be used
+  # - The project name
+  # - The set of outputs: static plots (with format) or shiny dashboard
 
   # Read the configuration file. Use deparse and assign functions to change the configuration global variable value
   configuration_tmp <- deparse(substitute(configuration))
   configuration     <- read_config_file(config_file,configuration)
   assign(configuration_tmp,configuration,pos=parent.frame())
 
-  # Check for reasonable inputs
+  # Check if the inputs are reasonable
   if(length(configuration$input_file_list) == 0) {
     stop("Input file list has no elements.
          \n  You must specify one or more files (separated by white spaces) in the config file after 'input_file_list:'.")
@@ -116,13 +127,14 @@ allergies_and_social_media <- function(config_file)
          \n  Please specify a project name under 'project_name:' in the config file.")
   }
 
-  # check if static plots format is one of the predefined formats
+  # Check if the specified static plots format is one of the predefined formats
   if(configuration$do_static_plots & !(configuration$static_plot_format %in% possible_static_plots_formats)) {
     all_formats <- paste(possible_static_plots_formats,collapse = ", ")
     stop(paste("Specified static plot output filetype ", configuration$static_plot_format,
                " is not a supported filetype.  (Plot output filetype can be one of ",all_formats,").",sep=""))
   }
 
+  # Print the current configuration
   print_config(configuration)
 
   # Create the output directory. Use deparse and assign functions to change the configuration global variable value
@@ -131,26 +143,31 @@ allergies_and_social_media <- function(config_file)
   assign(output_dir_tmp,output_dir,pos=parent.frame())
   if(!dir.exists(file.path(output_dir))) dir.create(output_dir,showWarnings = FALSE)
 
-  # Create the plots output directory. Use deparse and assign functions to change the configuration global variable value
+  # Create the static plots output directory. Use deparse and assign functions to change the configuration global variable value
   plots_output_dir_tmp <- deparse(substitute(plots_output_dir))
   plots_output_dir     <- paste(output_dir,"plots/",sep="")
   assign(plots_output_dir_tmp,plots_output_dir,pos=parent.frame())
   if(!dir.exists(file.path(plots_output_dir))) dir.create(plots_output_dir,showWarnings = FALSE)
 
-  # Set preprocessing image file name. Use deparse and assign functions to change the configuration global variable value
+  # Set the preprocessing image file name. Use deparse and assign functions to change the configuration global variable value
   image_preprocessing_tmp <- deparse(substitute(image_preprocessing))
   image_preprocessing     <- paste(output_dir,"Preprocessing.RData",sep="")
   assign(image_preprocessing_tmp,image_preprocessing,pos=parent.frame())
-  # Set analysis image file name. Use deparse and assign functions to change the configuration global variable value
+
+  # Set the analysis image file name. Use deparse and assign functions to change the configuration global variable value
   image_analysis_tmp <- deparse(substitute(image_analysis))
   image_analysis     <- paste(output_dir,"Waterfall.RData",sep="")
   assign(image_analysis_tmp,image_analysis,pos=parent.frame())
 
-  # data pre-processing
-  file_exists_preprocessing <- file.exists(image_preprocessing) # checks if preprocessing image file exists
+  #####################################
+  # Data pre-processing and labelling #
+  #####################################
+
+  # Check if preprocessing image file already exists
+  file_exists_preprocessing <- file.exists(image_preprocessing)
   generated_labelling       <- FALSE
   if(!file_exists_preprocessing | configuration$rerun_data_preprocessing) {
-    # if output image for preprocessing don't exits or the user request rerun_data_preprocessing, then run preprocessing + labelling
+    # if preprocessing image don't exits or if the user sets rerun_data_preprocessing to yes, then run preprocessing + labelling
     if(!file_exists_preprocessing) {
       cat("\n")
       cat(paste("Preprocessing .RData image file ",image_preprocessing," doesn't exist. Running preprocessing and labelling to generate it.","\n",sep=""))
@@ -161,8 +178,12 @@ allergies_and_social_media <- function(config_file)
       cat("\n")
     }
 
-    source("scripts/Preprocessing.R")  # Data pre-processing
-    source("scripts/Waterfall.R")      # Data labelling
+    # Data preprocessing script
+    source("scripts/Preprocessing.R")
+
+    # Data labelling script
+    source("scripts/Waterfall.R")
+
     generated_labelling <- TRUE
   } else {
     cat("\n")
@@ -170,9 +191,10 @@ allergies_and_social_media <- function(config_file)
     cat("\n")
   }
 
-  file_exists_analysis <- file.exists(image_analysis) # checks if analysis image file exists
+  # Checks if labellig image file exists
+  file_exists_analysis <- file.exists(image_analysis)
   if(!file_exists_analysis | (configuration$rerun_data_labelling & !generated_labelling)) {
-    # if output image for labelling don't exits or the user request rerun_data_labelling, then run labelling only
+    # if labelling image for labelling don't exits or if the user sets rerun_data_labelling to yes, then run labelling only
     if(!file_exists_analysis) {
       cat("\n")
       cat(paste("Labelling .RData image file ",image_analysis," doesn't exist. Running labelling to generate it.","\n",sep=""))
@@ -183,27 +205,36 @@ allergies_and_social_media <- function(config_file)
       cat("\n")
     }
 
-    source("scripts/Waterfall.R")      # Data labelling
+    # Data labelling script
+    source("scripts/Waterfall.R")
+
   } else if(!generated_labelling) {
     cat("\n")
     cat(paste("Labelling .RData image file ",image_analysis," exists and rerun_data_labelling is set to 'no'. Passing to next pipe-line step.","\n",sep=""))
     cat("\n")
   }
 
-  # set the format of static plots
+  # Set the format of static plots
   output_format <<- configuration$static_plot_format
 
   # Static plotting
   if(configuration$do_static_plots) {
     # Producing static plots
 
-    source("scripts/plots.R")             # summary plots
-    source("scripts/Static_Geoplots.R")   # geographical differences plots
+    # Summary plots script
+    source("scripts/plots.R")
+
+    # Map based plots script
+    source("scripts/Static_Geoplots.R")
   }
 
+  # Shiny Dashboard
   if(configuration$launch_shiny_dash_board) {
     # Launching shiny dashboard
+
     library(shiny)
+
+    # launching shiny app
     runApp("Dashboard")
   }
 
